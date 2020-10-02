8026afdc0c13f1005ee42186287ad101fc1b6099
JayGupGup <jayguptar@live.nl>
2020-10-02 11:28:49 +0200

    changed sun color

--- src/Four/two.java
+++ src/Four/two.java
@@ -12,6 +12,7 @@ public class two extends Applet {
         g.drawPolygon(new int[] {250,150, 350}, new int[] {0, 150, 150}, 3);
         g.drawRect(175, 280, 40,70);
         g.drawRect(240, 240, 80, 80);
+        g.setColor(Color.YELLOW);
         g.fillOval(20,20,40,40);
         g.drawOval(20,20,40,40);
 
