93d14c81eb58234fd5eec5fc43210274badb903f
JayGupGup <jayguptar@live.nl>
2020-10-02 11:22:43 +0200

    house added

--- src/Four/two.java
+++ src/Four/two.java
@@ -10,6 +10,10 @@ public class two extends Applet {
     public void paint(Graphics g){
         g.drawRect(150,150,200,200);
         g.drawPolygon(new int[] {250,150, 350}, new int[] {0, 150, 150}, 3);
-        g.drawRect(175, 310, 30,60);
+        g.drawRect(175, 280, 40,70);
+        g.drawRect(240, 240, 80, 80);
+        g.fillOval(20,20,40,40);
+        g.drawOval(20,20,40,40);
+
     }
 }
