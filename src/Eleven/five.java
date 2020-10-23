64cb9654058d06404902fccf009e065c4c1dca44
JayGupGup <jayguptar@live.nl>
2020-10-23 16:22:47 +0200

    h11

--- src/Eleven/five.java
+++ src/Eleven/five.java
@@ -1,4 +1,25 @@
 package Eleven;
 
-public class five {
+import java.applet.Applet;
+import java.awt.*;
+
+public class five extends Applet {
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        int teller = 0;
+        int width = 50;
+        int height = 50;
+        int x = 250;
+        int y = 250;
+
+        while (teller < 5) {
+            g.drawRect(x, y, width, height);
+            x += 50;
+            y += 50;
+
+            teller++;
+        }
+    }
 }
--- src/Eleven/praktijkopdracht.java
+++ src/Eleven/praktijkopdracht.java
@@ -1,19 +1,43 @@
 package Eleven;
 
+
+
+import Six.Praktijkopdracht;
+
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class praktijkopdracht extends Applet {
     TextField tekstvak;
-    String invoer;
+    int invoer;
     int teller;
-    int y;
+    int y = 0;
+    String uitkomst;
+    Button enter;
+
     public void init() {
+        tekstvak = new TextField("", 20);
+        add(tekstvak);
+        uitkomst = "";
+        enter = new Button("=");
+        enter.addActionListener(new praktijkopdracht.enterlistener());
 
     }
 
     public void paint(Graphics g) {
+        while (teller < 10) {
+            y =+ 20;
+            g.drawString("De tafel van " + invoer + " = ", 50, 50);
+            teller =+invoer;
+            teller++;
+        }
+
 
     }
-
+     class enterlistener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+        }
+    }
 }
