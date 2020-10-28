1d61924d417a096d748dac3f6cdb357b770d1e43
JayGupGup <jayguptar@live.nl>
2020-10-28 11:06:43 +0100

    12,3

---
+++ src/Twelve/four.java
@@ -0,0 +1,13 @@
+package Twelve;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class four extends Applet {
+    TextField tekstvak[];
+    Button knop;
+    public void init(){
+        knop = new Button("");
+
+    }
+}
--- src/Twelve/three.java
+++ src/Twelve/three.java
@@ -1,6 +1,5 @@
 package Twelve;
 
-import Ten.four;
 
 import java.applet.*;
 import java.awt.*;
@@ -10,38 +9,38 @@ import java.util.*;
 
 
 public class three extends Applet {
-    TextField tekstvak[] = new TextField[5];
+    TextField[] tekstvak = new TextField[5];
+    int[] sorter = new int[5];
     Button ok;
-    String s;
 
     public void init() {
-        Arrays.sort(tekstvak);
-        int teller = 0;
 
-        while (teller < 5) {
-            tekstvak[4] = new TextField("");
-            add(tekstvak[4]);
-            teller++;
+
+        for (int teller = 0; teller < 5; teller++) {
+            tekstvak[teller] = new TextField("",10);
+            add(tekstvak[teller]);
+
         }
         ok = new Button("OK");
         ok.addActionListener(new okListener());
         add(ok);
-        s = "";
-        tekstvak.addActionListener(new three.tekstvakListener());
+
+
     }
-    public void paint(Graphics g){
-        g.drawString("",50,50);
-    }
+
 
     class okListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
 
-            repaint();
-        }
-    }
-    class tekstvakListener implements ActionListener{
-        public void actionPerformed(ActionEvent e){
-            s = tekstvak.getText();
+            for (int teller = 0; teller < 5; teller++) {
+                sorter[teller] = Integer.parseInt(tekstvak[teller].getText());
+            }
+            Arrays.sort(sorter);
+
+            for (int i = 0; i < 5; i++) {
+                tekstvak[i].setText("" + (sorter[i]));
+
+            }
         }
     }
 }
