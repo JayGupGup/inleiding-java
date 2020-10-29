635d484e668da6a8678b0eec9471c252400f2b6c
JayGupGup <jayguptar@live.nl>
2020-10-29 14:46:38 +0100

    h13

--- src/Thirteen/Praktijkopdracht.java
+++ src/Thirteen/Praktijkopdracht.java
@@ -9,11 +9,11 @@ public class Praktijkopdracht extends Applet {
         setSize(600, 500);
     }
 
-    void tekenboom(Graphics g, int x1, int y1, int width1, int heigt1, int x2, int y2, int width2, int heigt2) {
+    void tekenboom(Graphics g, int x1, int y1, int width1, int heigt1) {
         g.setColor(new Color( 165, 42,42));
         g.fillRect(x1, y1, width1, heigt1);
         g.setColor(Color.GREEN);
-        g.fillOval(x2, y2, width2, heigt2);
+        g.fillOval(x1-20, y1-20, width1*2, heigt1/2);
     }
 
     void tekenbos(Graphics g, int width, int heigt, int width2, int heigt2, int rij, int kolom) {
@@ -21,10 +21,10 @@ public class Praktijkopdracht extends Applet {
             for (int i = 0; i < rij; i++) {
 
                 if (j % 1 == 0) {
-                    tekenboom(g, 50 + (i * width), 150 + (j * heigt), width, heigt, 35 + (i * width), 130 + (j * heigt), width2, heigt2);
+                    tekenboom(g, 50 + (i * (width+20)), 150 + (j * heigt), width, heigt);
                 } else {
 
-                    tekenboom(g, 50 + (width / 2) + (i * width), 150 + (j * heigt), width, heigt, 35 + (width / 2) + (i * width), 130 + (j * heigt), width2, heigt2);
+                    tekenboom(g, 50 + (width / 2) + (i * width), 150 + (j * heigt), width, heigt);
                 }
             }
         }
@@ -32,7 +32,7 @@ public class Praktijkopdracht extends Applet {
 
     public void paint(Graphics g) {
 
-        tekenboom(g, 50, 150, 30, 130, 35, 130, 60, 60);
+        tekenboom(g, 50, 150, 30, 130);
         tekenbos(g, 30, 130, 60, 60, 5, 2);
     }
 }
