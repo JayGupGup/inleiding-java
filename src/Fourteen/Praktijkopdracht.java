8ee478c40de5c4eaebca2efdebebc5ad49e48738
JayGupGup <jayguptar@live.nl>
2020-11-14 02:04:40 +0100

    H14,2

--- src/Fourteen/Praktijkopdracht.java
+++ src/Fourteen/Praktijkopdracht.java
@@ -14,10 +14,12 @@ public class Praktijkopdracht extends Applet {
     private URL pad;
     double y;
     String uitkomst;
-
+    int totalNiggario;
+    int columnWidth = 5;
+    String onjuisteinvoer;
 
     public void init() {
-        setSize(700,650);
+        setSize(700, 650);
         tekstvak = new TextField("", 20);
         speel = new Button("Speel");
         speel.addActionListener(new knopListener());
@@ -27,34 +29,43 @@ public class Praktijkopdracht extends Applet {
         y = 1.5;
         add(speel);
         add(tekstvak);
+        totalNiggario = 23;
     }
 
     void tekenNiggario(Graphics g, int x, int y, int width, int height) {
         pad = Praktijkopdracht.class.getResource("/resources/");
         niggario = getImage(pad, "niggario.png");
         g.drawImage(niggario, x, y, width, height, this);
-
     }
 
-    void tekenniggarios(Graphics g, int width, int heigt, int rij, int kolom) {
-        for (int j = 0; j < kolom; j++) {
-            for (int i = 0; i < rij; i++) {
-
+    void tekenniggarios(Graphics g, int width, int heigt, int rij, int kolom, int total) {
+        int leftOvers = total % kolom;
+        boolean laatMaar; // = (leftOvers == 0) ? false : true; //ternary
+        if (leftOvers == 0) {
+            laatMaar = false;
+        } else {
+            laatMaar = true;
+        }
+        if (leftOvers != 0) rij++;
+        System.out.println(leftOvers);
+        for (int j = 0; j < rij; j++) {
+            for (int i = 0; i < kolom; i++) {
+                if (j == (rij - 1) && leftOvers != 0) leftOvers--;
                 if (j % 1 == 0) {
                     tekenNiggario(g, 50 + (i * (width + 20)), (int) (70 + (j * heigt * y)), width, heigt);
                 } else {
-
                     tekenNiggario(g, 50 + (width / 2) + (i * width), 70 + (j * heigt), width, heigt);
                 }
+                if (laatMaar && leftOvers == 0) break;
             }
         }
     }
 
     public void paint(Graphics g) {
-        tekenNiggario(g, 50, 520, 100, 75);
-        tekenNiggario(g, 165, 520, 100, 75);
-        tekenNiggario(g, 290, 520, 100, 75);
-        tekenniggarios(g, 100, 75, 5, 4);
+        int rows = (int) totalNiggario / columnWidth;
+        tekenniggarios(g, 100, 75, rows, columnWidth, totalNiggario);
+        g.setColor(Color.RED);
+        g.drawString("" + onjuisteinvoer, 330, 45);
     }
 
 
@@ -64,13 +75,15 @@ public class Praktijkopdracht extends Applet {
             int invoer;
             invoer = Integer.parseInt(uitkomst);
             if (invoer < 4) {
-
+                totalNiggario -= invoer;
+                System.out.println(totalNiggario);
+                Math.random();
             } else {
-                getGraphics().setColor(Color.RED);
-                getGraphics().drawString("Onjuiste invoer",330,45);
+
+
             }
 
-
+            repaint();
         }
 
     }
